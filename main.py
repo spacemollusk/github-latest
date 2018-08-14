8b98420281d201f272fa4db4fb521178ea52e091
Jim Blackburn <jimbb82@uw.edu>
2018-08-14 16:28:17 -0700

    Successfully implemented retrieval of user event timestamp

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ import json
 
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
-
 if __name__ == "__main__":
     username = sys.argv[1]
-
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
-
+    request = "https://api.github.com/users/{}/events".format(username)
+    response = requests.get(request)
+    events = response.json()
+    print(events[0]['created_at'])
